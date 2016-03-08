0c1119874c125b34ea6ad336592e35f6e05049e8
Pablo Pareja <pabloparejatobes@gmail.com>
2016-03-08 14:54:59 +0100

    making some progress

--- src/main/java/org/sandbag/eutlws/EUTLSeleniumTest.java
+++ src/main/java/org/sandbag/eutlws/EUTLSeleniumTest.java
@@ -37,192 +37,212 @@ public class EUTLSeleniumTest {
                     "3. Output CSV file name period 2");
         }else{
 
+            getAllocationsToStationaryInstallations(args[0], args[1], args[2]);
 
-            File outputFilePeriod0 = new File(args[0]);
-            File outputFilePeriod1 = new File(args[1]);
-            File outputFilePeriod2 = new File(args[2]);
-
-            // Create a new instance of the Firefox driver
-            // Notice that the remainder of the code relies on the interface,
-            // not the implementation.
-            WebDriver driver = new FirefoxDriver();
-
-            BufferedWriter outBuffPeriod0 = new BufferedWriter(new FileWriter(outputFilePeriod0));
-            outBuffPeriod0.write(PERIOD_0_HEADER + "\n");
-            BufferedWriter outBuffPeriod1 = new BufferedWriter(new FileWriter(outputFilePeriod1));
-            outBuffPeriod1.write(PERIOD_1_HEADER + "\n");
-            BufferedWriter outBuffPeriod2 = new BufferedWriter(new FileWriter(outputFilePeriod2));
-            outBuffPeriod2.write(PERIOD_2_HEADER + "\n");
-
-
-            for(String country : countriesArray){
-                for(int period=0; period<=2; period++){
-                    // And now use this to visit Google
-                    driver.get("http://ec.europa.eu/environment/ets/nap.do?languageCode=en&nap.registryCodeArray="+
-                            country +"&periodCode=" + period + "&search=Search&currentSortSettings=");
-
-
-                    List<WebElement> tempElements = driver.findElements(By.id("lnkNapInformation"));
-                    WebElement elementToBeClicked = null;
-
-                    for(WebElement element : tempElements){
-                        //System.out.println("element = " + element);
-                        String tempHrefl = element.getAttribute("href");
-                        System.out.println("tempHrefl = " + tempHrefl);
-
-                        WebElement spanElement = element.findElement(By.tagName("span"));
-                        String spanText = spanElement.getText();
-                        System.out.println("spanText = " + spanText);
-                        if(spanText.equals("Installations linked to this Allocation Table")){
-                            elementToBeClicked = element;
-                        }
-                    }
-
-                    if(elementToBeClicked != null){
-                        // Now submit the form. WebDriver will find the form for us from the element
-                        elementToBeClicked.click();
-
-                        Thread.sleep(1000);
-
-                        WebElement table_element = driver.findElement(By.id("tblNapList"));
-
-                        //-----------------------TABLE ELEMENT-------------------------------------------------------------
-
-                        int pageNumber = 0;
-
-                        while(table_element != null){
-
-                            System.out.println("Country: " + country + " Period: " + period + " Page: " + pageNumber);
-
-                            List<WebElement> tr_collection=table_element.findElements(By.xpath("id('tblNapList')/tbody/tr"));
-
-                            if(period == 0){
-
-                                for(int rowCounter=3; rowCounter<tr_collection.size(); rowCounter++)
-                                {
-
-                                    outBuffPeriod0.write(country + "\t");
-
-                                    WebElement trElement = tr_collection.get(rowCounter);
-
-                                    List<WebElement> td_collection=trElement.findElements(By.xpath("td"));
-
-                                    for(int columnCounter=0; columnCounter< td_collection.size()-1;columnCounter++)
-                                    {
-
-                                        WebElement tdElement = td_collection.get(columnCounter);
-
-                                        outBuffPeriod0.write(tdElement.getText().replaceAll("\n"," "));
-
-                                        if(columnCounter == 10){
-                                            outBuffPeriod0.write("\n");
-                                        }else{
-                                            outBuffPeriod0.write("\t");
-                                        }
-
-                                    }
-
-                                    System.out.println("Row: " + rowCounter + " completed");
-                                }
-
-                            }else if(period == 1){
-
-                                for(int rowCounter=3; rowCounter<tr_collection.size(); rowCounter++)
-                                {
-
-                                    outBuffPeriod1.write(country + "\t");
-
-                                    WebElement trElement = tr_collection.get(rowCounter);
-
-                                    List<WebElement> td_collection=trElement.findElements(By.xpath("td"));
-                                    //System.out.println("NUMBER OF COLUMNS="+td_collection.size());
-
-
-
-                                    for(int columnCounter=0; columnCounter< td_collection.size()-1;columnCounter++)
-                                    {
-
-                                        WebElement tdElement = td_collection.get(columnCounter);
-
-                                        outBuffPeriod1.write(tdElement.getText().replaceAll("\n"," "));
-                                        if(columnCounter == 12){
-                                            outBuffPeriod1.write("\n");
-                                        }else{
-                                            outBuffPeriod1.write("\t");
-                                        }
-
-                                    }
-
-                                    System.out.println("Row: " + rowCounter + " completed");
-                                }
-                            }else if(period == 2){
-
-                                for(int rowCounter=3; rowCounter<(tr_collection.size()-3); rowCounter++)
-                                {
-
-                                    outBuffPeriod2.write(country + "\t");
-
-                                    WebElement trElement = tr_collection.get(rowCounter);
-
-                                    List<WebElement> td_collection=trElement.findElements(By.xpath("td"));
-                                    //System.out.println("NUMBER OF COLUMNS="+td_collection.size());
-
-                                    for(int columnCounter=0; columnCounter< td_collection.size()-1;columnCounter++)
-                                    {
-
-                                        WebElement tdElement = td_collection.get(columnCounter);
-
-                                        outBuffPeriod2.write(tdElement.getText().replaceAll("\n"," "));
-                                        if(columnCounter == 15){
-                                            outBuffPeriod2.write("\n");
-                                        }else{
-                                            outBuffPeriod2.write("\t");
-                                        }
-
-                                    }
-
-                                    System.out.println("Row: " + rowCounter + " completed");
-                                }
-                            }
-
-
-
-
-                            //----------------NEXT PAGE OF RESULTS BUTTON-----------------------
-                            WebElement nextButton = driver.findElement(By.name("nextList"));
-                            if(nextButton.getAttribute("disabled") == null){
-                                nextButton.click();
-                                table_element = driver.findElement(By.id("tblNapList"));
-                            }else{
-                                table_element = null;
-                            }
-
-                            pageNumber++;
-
-                        }
-
-                        outBuffPeriod0.flush();
-                        outBuffPeriod1.flush();
-                        outBuffPeriod2.flush();
-
-                        //-----------------------------------------------------------------------------------------
-                    }
-
-                }
-            }
-
-            //Close the browser
-            driver.quit();
-
-            //Close output files
-            outBuffPeriod0.close();
-            outBuffPeriod1.close();
-            outBuffPeriod2.close();
         }
 
 
 
 
+
+    }
+
+    public static void getOperatorHoldingAccounts(String installationsFileSt) throws Exception{
+
+        File installationsFile = new File(installationsFileSt);
+        BufferedWriter outBuff = new BufferedWriter(new FileWriter(installationsFile));
+
+
+        outBuff.close();
+        
+
+    }
+
+    public static void getAllocationsToStationaryInstallations(
+            String filePeriod0St,
+            String filePeriod1St,
+            String filePeriod2St) throws Exception{
+
+        File outputFilePeriod0 = new File(filePeriod0St);
+        File outputFilePeriod1 = new File(filePeriod1St);
+        File outputFilePeriod2 = new File(filePeriod2St);
+
+        // Create a new instance of the Firefox driver
+        // Notice that the remainder of the code relies on the interface,
+        // not the implementation.
+        WebDriver driver = new FirefoxDriver();
+
+        BufferedWriter outBuffPeriod0 = new BufferedWriter(new FileWriter(outputFilePeriod0));
+        outBuffPeriod0.write(PERIOD_0_HEADER + "\n");
+        BufferedWriter outBuffPeriod1 = new BufferedWriter(new FileWriter(outputFilePeriod1));
+        outBuffPeriod1.write(PERIOD_1_HEADER + "\n");
+        BufferedWriter outBuffPeriod2 = new BufferedWriter(new FileWriter(outputFilePeriod2));
+        outBuffPeriod2.write(PERIOD_2_HEADER + "\n");
+
+
+        for(String country : countriesArray){
+            for(int period=0; period<=2; period++){
+                // And now use this to visit Google
+                driver.get("http://ec.europa.eu/environment/ets/nap.do?languageCode=en&nap.registryCodeArray="+
+                        country +"&periodCode=" + period + "&search=Search&currentSortSettings=");
+
+
+                List<WebElement> tempElements = driver.findElements(By.id("lnkNapInformation"));
+                WebElement elementToBeClicked = null;
+
+                for(WebElement element : tempElements){
+                    //System.out.println("element = " + element);
+                    String tempHrefl = element.getAttribute("href");
+                    System.out.println("tempHrefl = " + tempHrefl);
+
+                    WebElement spanElement = element.findElement(By.tagName("span"));
+                    String spanText = spanElement.getText();
+                    System.out.println("spanText = " + spanText);
+                    if(spanText.equals("Installations linked to this Allocation Table")){
+                        elementToBeClicked = element;
+                    }
+                }
+
+                if(elementToBeClicked != null){
+                    // Now submit the form. WebDriver will find the form for us from the element
+                    elementToBeClicked.click();
+
+                    Thread.sleep(1000);
+
+                    WebElement table_element = driver.findElement(By.id("tblNapList"));
+
+                    //-----------------------TABLE ELEMENT-------------------------------------------------------------
+
+                    int pageNumber = 0;
+
+                    while(table_element != null){
+
+                        System.out.println("Country: " + country + " Period: " + period + " Page: " + pageNumber);
+
+                        List<WebElement> tr_collection=table_element.findElements(By.xpath("id('tblNapList')/tbody/tr"));
+
+                        if(period == 0){
+
+                            for(int rowCounter=3; rowCounter<tr_collection.size(); rowCounter++)
+                            {
+
+                                outBuffPeriod0.write(country + "\t");
+
+                                WebElement trElement = tr_collection.get(rowCounter);
+
+                                List<WebElement> td_collection=trElement.findElements(By.xpath("td"));
+
+                                for(int columnCounter=0; columnCounter< td_collection.size()-1;columnCounter++)
+                                {
+
+                                    WebElement tdElement = td_collection.get(columnCounter);
+
+                                    outBuffPeriod0.write(tdElement.getText().replaceAll("\n"," "));
+
+                                    if(columnCounter == 10){
+                                        outBuffPeriod0.write("\n");
+                                    }else{
+                                        outBuffPeriod0.write("\t");
+                                    }
+
+                                }
+
+                                System.out.println("Row: " + rowCounter + " completed");
+                            }
+
+                        }else if(period == 1){
+
+                            for(int rowCounter=3; rowCounter<tr_collection.size(); rowCounter++)
+                            {
+
+                                outBuffPeriod1.write(country + "\t");
+
+                                WebElement trElement = tr_collection.get(rowCounter);
+
+                                List<WebElement> td_collection=trElement.findElements(By.xpath("td"));
+                                //System.out.println("NUMBER OF COLUMNS="+td_collection.size());
+
+
+
+                                for(int columnCounter=0; columnCounter< td_collection.size()-1;columnCounter++)
+                                {
+
+                                    WebElement tdElement = td_collection.get(columnCounter);
+
+                                    outBuffPeriod1.write(tdElement.getText().replaceAll("\n"," "));
+                                    if(columnCounter == 12){
+                                        outBuffPeriod1.write("\n");
+                                    }else{
+                                        outBuffPeriod1.write("\t");
+                                    }
+
+                                }
+
+                                System.out.println("Row: " + rowCounter + " completed");
+                            }
+                        }else if(period == 2){
+
+                            for(int rowCounter=3; rowCounter<(tr_collection.size()-3); rowCounter++)
+                            {
+
+                                outBuffPeriod2.write(country + "\t");
+
+                                WebElement trElement = tr_collection.get(rowCounter);
+
+                                List<WebElement> td_collection=trElement.findElements(By.xpath("td"));
+                                //System.out.println("NUMBER OF COLUMNS="+td_collection.size());
+
+                                for(int columnCounter=0; columnCounter< td_collection.size()-1;columnCounter++)
+                                {
+
+                                    WebElement tdElement = td_collection.get(columnCounter);
+
+                                    outBuffPeriod2.write(tdElement.getText().replaceAll("\n"," "));
+                                    if(columnCounter == 15){
+                                        outBuffPeriod2.write("\n");
+                                    }else{
+                                        outBuffPeriod2.write("\t");
+                                    }
+
+                                }
+
+                                System.out.println("Row: " + rowCounter + " completed");
+                            }
+                        }
+
+
+
+
+                        //----------------NEXT PAGE OF RESULTS BUTTON-----------------------
+                        WebElement nextButton = driver.findElement(By.name("nextList"));
+                        if(nextButton.getAttribute("disabled") == null){
+                            nextButton.click();
+                            table_element = driver.findElement(By.id("tblNapList"));
+                        }else{
+                            table_element = null;
+                        }
+
+                        pageNumber++;
+
+                    }
+
+                    outBuffPeriod0.flush();
+                    outBuffPeriod1.flush();
+                    outBuffPeriod2.flush();
+
+                    //-----------------------------------------------------------------------------------------
+                }
+
+            }
+        }
+
+        //Close the browser
+        driver.quit();
+
+        //Close output files
+        outBuffPeriod0.close();
+        outBuffPeriod1.close();
+        outBuffPeriod2.close();
 
     }
 
